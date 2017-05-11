bbe29732a1ca2df9870fe19b2ba02cc12635bbe3
jeffsga88 <jmortensen1@my.devry.edu>
2017-05-11 11:36:01 -0500

    Added zen decision hotplugging

--- include/linux/msm_kgsl.h
+++ include/linux/msm_kgsl.h
@@ -10,4 +10,5 @@ int kgsl_pwr_limits_set_freq(void *limit, unsigned int freq);
 void kgsl_pwr_limits_set_default(void *limit);
 unsigned int kgsl_pwr_limits_get_freq(enum kgsl_deviceid id);
 
+
 #endif /* _MSM_KGSL_H */
