ff0e486b7aaf9dba3c98fbff6a842a8e7fb57c0c
jeffsga88 <jmortensen1@my.devry.edu>
2017-05-10 21:20:24 -0500

    Added TCP (Vegas, Westwood, Illinois) and changed default Scheduler to Zen

---
+++ build_anykernel.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Copyright - CrazyGamerGR
+#     		 ________     __   ___
+#    		/ ______|    |  | /  |
+#    		|  |         |  |/  /
+#   		|  |_____    |      \              
+#   		\_______|    |__|\___|
+##########################################
+export ARCH=arm64
+export CROSS_COMPILE=~/android/toolchain/los-aarch64-linux-android-4.9/bin/aarch64-linux-android-
+##########################################
+make clean && make mrproper
+
+rmdir output830
+rmdir output850
+
+mkdir output830
+mkdir output850
+##########################################
+make -C $(pwd) O=output830 speedykernel_h830_defconfig -j5
+make -C $(pwd) O=output830 -j5
+
+#make -C $(pwd) O=output850 speedykernel_h850_defconfig -j5
+#make -C $(pwd) O=output850 -j5
+##########################################
+cp output830/arch/arm64/boot/Image.gz-dtb $(pwd)/arch/arm64/boot/zImage-dtb
+#cp output850/arch/arm64/boot/Image.gz-dtb $(pwd)/arch/arm64/boot/zImage-dtb
