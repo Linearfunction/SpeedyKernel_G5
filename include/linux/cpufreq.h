8ef97f98c12bfcb0afeb059720f5ef2dd86f1868
jeffsga88 <jmortensen1@my.devry.edu>
2017-05-17 13:42:37 -0500

    Added Nebula Gov

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -522,6 +522,9 @@ extern struct cpufreq_governor cpufreq_gov_powersave;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
 extern struct cpufreq_governor cpufreq_gov_alucard;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_alucard)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NEBULA)
+extern struct cpufreq_governor cpufreq_gov_nebula;
+define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_nebula)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
 extern struct cpufreq_governor cpufreq_gov_lionheart;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_LIONHEART)
