e448e0fbdaa5fe42920615f44f1e2c99badb2a02
jeffsga88 <jmortensen1@my.devry.edu>
2017-06-15 15:16:22 -0500

    Revert "Change interactive gov to old_interactive and interactive_pro to interactive until interactive is fixed, AKT now works :)"
    This reverts commit 5bbd81b35ec9580e784e8a2eb3887f45ae7ef193.

--- rebuild.sh
+++ rebuild.sh
@@ -11,15 +11,15 @@
 # --- --- Modified by jeffsga88 --- ---  #
 ##########################################
 export ARCH=arm64
-export CROSS_COMPILE=~/android/toolchain/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=~/android/toolchain/los-aarch64-linux-android-4.9/bin/aarch64-linux-android-
 ##########################################
 
 ##########################################
-make -C $(pwd) O=output830 speedykernel_h830_defconfig -j9
-make -C $(pwd) O=output830 -j9
+make -C $(pwd) O=output830 speedykernel_h830_defconfig -j5
+make -C $(pwd) O=output830 -j5
 
-# make -C $(pwd) O=output850 speedykernel_h850_defconfig -j9
-# make -C $(pwd) O=output850 -j9
+# make -C $(pwd) O=output850 speedykernel_h850_defconfig -j5
+# make -C $(pwd) O=output850 -j5
 ##########################################
 cp output830/arch/arm64/boot/Image.gz-dtb $(pwd)/arch/arm64/boot/zImage-dtb
 # cp output850/arch/arm64/boot/Image.gz-dtb $(pwd)/arch/arm64/boot/zImage-dtb
